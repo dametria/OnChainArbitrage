f4148473d4a16fbb4d00361f8178b6d04dfe876f
dametria <39619278+dametria@users.noreply.github.com>
2026-07-30 00:31:32 -0500

    Update dynamicPairs.ts

--- src/dynamicPairs.ts
+++ src/dynamicPairs.ts
@@ -37,7 +37,7 @@ export interface TradingPairsConfig {
   }>;
 }
 
-const PAIRS_FILE_PATH = path.join('..', 'data', 'pairs', 'trading-pairs.json');
+const PAIRS_FILE_PATH = path.join('.', 'data', 'pairs', 'trading-pairs.json');
 
 /**
  * Load trading pairs from JSON file
