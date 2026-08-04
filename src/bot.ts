6bce6fbb26796c1e1785a1312e31fc9c3a163031
dametria <39619278+dametria@users.noreply.github.com>
2026-08-03 22:33:10 -0500

    Update bot.ts

--- src/bot.ts
+++ src/bot.ts
@@ -17,7 +17,7 @@
  */
 
 import { ethers } from "ethers";
-import { config, validateConfig } from "./config";
+import { config, validateConfig } from "./config.ts";
 import { logger } from "./logger";
 import { PriceMonitor } from "./priceMonitor";
 import { TradeExecutor } from "./tradeExecutor";
