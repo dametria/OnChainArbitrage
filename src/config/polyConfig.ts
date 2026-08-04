2a38f02c608a5739360952ac565ff8eb9b262a29
dametria <39619278+dametria@users.noreply.github.com>
2026-08-03 23:00:00 -0500

    Update polyConfig.ts

--- src/config/polyConfig.ts
+++ src/config/polyConfig.ts
@@ -7,11 +7,8 @@ export const polyConfig = {
     name: "polygon" as const,
     chainId: 137,
     rpcUrl:
-      process.env.POLYGON_RPC_URL || "https://polygon-mainnet.infura.io/v3/0943af620e824e12a62823f73eacc3f5",
-      process.env.RPC_URL || "https://polygon-mainnet.infura.io/v3/0943af620e824e12a62823f73eacc3f5",
-    rpcUrl:
-      process.env.POLYGON_WSS_URL || "wss://polygon-mainnet.infura.io/ws/v3/0943af620e824e12a62823f73eacc3f5",
-      process.env.RPC_WSS_URL || "wss://polygon-mainnet.infura.io/ws/v3/0943af620e824e12a62823f73eacc3f5",
+     http: process.env.POLYGON_RPC_URL || "https://polygon-mainnet.infura.io/v3/0943af620e824e12a62823f73eacc3f5",
+    websocket: process.env.POLYGON_WSS_URL || "wss://polygon-mainnet.infura.io/ws/v3/0943af620e824e12a62823f73eacc3f5",
   },
 
   contracts: {
