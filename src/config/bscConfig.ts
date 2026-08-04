6c96045823de204d1509b7d6b0d364ed1b619bf8
dametria <39619278+dametria@users.noreply.github.com>
2026-08-03 23:06:52 -0500

    Update bscConfig.ts

--- src/config/bscConfig.ts
+++ src/config/bscConfig.ts
@@ -11,8 +11,8 @@ export const bscConfig = {
     name: "bsc" as const,
     chainId: 56,
     rpcUrl:
-     https: process.env.BSC_RPC_URL || "https://bsc-mainnet.infura.io/v3/0943af620e824e12a62823f73eacc3f5",
-    websocket: process.env.BSC_WSS_URL || "wss://bsc-mainnet.infura.io/ws/v3/0943af620e824e12a62823f73eacc3f5",
+      process.env.BSC_RPC_URL || "https://bsc-mainnet.infura.io/v3/0943af620e824e12a62823f73eacc3f5",
+    rpcWssUrl: process.env.BSC_WSS_URL || "wss://bsc-mainnet.infura.io/ws/v3/0943af620e824e12a62823f73eacc3f5",
   },
 
   contracts: {
