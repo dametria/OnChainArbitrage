a01f03b7ce264b05d9086db5ad4cbb186db93cdb
dametria <39619278+dametria@users.noreply.github.com>
2026-08-03 23:48:35 -0500

    Update dynamicPairs.ts

--- src/dynamicPairs.ts
+++ src/dynamicPairs.ts
@@ -8,9 +8,9 @@
 
 import * as fs from "fs";
 import * as path from "path";
-import { polyConfig } from "./config/polyConfig";
-import { bscConfig } from "./config/bscConfig";
-import { baseConfig } from "./config/baseConfig";
+import { polyConfig } from "./chainConfigs/polyConfig";
+import { bscConfig } from "./chainConfigs/bscConfig";
+import { baseConfig } from "./chainConfigs/baseConfig";
 
 export interface TradingPair {
   name: string;
