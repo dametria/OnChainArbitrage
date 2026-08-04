10eff40b1b19ed0bc6bc5aae1b6ff0400c3ac751
dametria <39619278+dametria@users.noreply.github.com>
2026-08-03 23:58:31 -0500

    Update import statement to use config.js

--- src/bot.ts
+++ src/bot.ts
@@ -17,7 +17,7 @@
  */
 
 import { ethers } from "ethers";
-import { config, validateConfig } from "./config";
+import { config, validateConfig } from "./config.js";
 import { logger } from "./logger";
 import { PriceMonitor } from "./priceMonitor";
 import { TradeExecutor } from "./tradeExecutor";
